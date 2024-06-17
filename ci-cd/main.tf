4e4ddfc7388b4d02e12c164853a506096242f927
vbr-encora <vicente.borja@encora.com>
2024-06-17 11:40:46 -0600

    test deploy to ecs

--- ci-cd/main.tf
+++ ci-cd/main.tf
@@ -97,3 +97,7 @@ resource "aws_iam_role_policy_attachment" "codebuild_ecr_policy_attachment" {
   role       = aws_iam_role.codebuild_role.name
 }
 
+resource "aws_iam_role_policy_attachment" "codebuild_ecs_policy_attachment" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
+}
